45712c993870fc023d3caffc2dcc26ddb68d30b4
Alex Snow <alexsnow348@gmail.com>
2025-02-14 10:14:24 +0100

    updated to include multiple folder

--- server.mjs
+++ server.mjs
@@ -1,15 +1,20 @@
-import express from 'express';
-import path from 'path';
-import fs from 'fs';
-import cors from 'cors';
+import express from "express";
+import path from "path";
+import fs from "fs";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 
 // Define OS-specific image directory
-const isWindows = process.platform === 'win32';
-const IMAGE_DIR = isWindows ? 'C:/images' : '/data/biolab_arralyze_picture_data/';
-const INTERESTED_FOLDER = ["20240529 K562-NK cells purified_Killing Assay (9b33)", "20241219_Hek293 trypsination timelapse (3826)"];	
+const isWindows = process.platform === "win32";
+const IMAGE_DIR = isWindows
+  ? "C:/images"
+  : "/data/biolab_arralyze_picture_data/";
+const INTERESTED_FOLDER = [
+  "20240529 K562-NK cells purified_Killing Assay (9b33)",
+  "20241219_Hek293 trypsination timelapse (3826)",
+];
 const IMAGES = [];
 app.use(cors()); // Enable CORS for frontend access
 
@@ -28,11 +33,9 @@ async function scanDirectory(dir) {
     } else if (stats.isFile() && /\.(jpg|jpeg|png|gif|webp)$/i.test(file)) {
       // If it's an image file, add to the list and the filename include Merged
 
-      if (fullPath.includes('Merged')) {
+      if (fullPath.includes("Merged")) {
         imageFiles.push(fullPath);
       }
-  
-      
     }
   }
 
@@ -47,20 +50,21 @@ for (const folder of INTERESTED_FOLDER) {
 
 // const IMAGES = await scanDirectory(IMAGE_DIR);
 // API to list images in the directory (including subfolders)
-app.get('/images/list', async (req, res) => {
+app.get("/images/list", async (req, res) => {
   try {
     const experimentName = req.query.experimentName;
     // Only return the image filenames (not the full path) and experiment name
-    const imageFiles = IMAGES.filter(image => path.relative(IMAGE_DIR, image).includes(experimentName))
-                             .map(image => path.relative(IMAGE_DIR, image));
+    const imageFiles = IMAGES.filter((image) =>
+      path.relative(IMAGE_DIR, image).includes(experimentName)
+    ).map((image) => path.relative(IMAGE_DIR, image));
     res.json(imageFiles);
   } catch (err) {
-    res.status(500).json({ error: 'Unable to scan directory' });
+    res.status(500).json({ error: "Unable to scan directory" });
   }
 });
 
 // Serve images from the OS-specific directory
-app.use('/images', express.static(IMAGE_DIR));
+app.use("/images", express.static(IMAGE_DIR));
 
 app.listen(PORT, () => {
   console.log(`Image server running at http://localhost:${PORT}`);
